73cbbffbc500c02c920e7efbb66e00c4768bc427
Karl Olson <76186248+KarlOlson@users.noreply.github.com>
2023-10-02 12:21:00 -0400

    Update proxy.py

--- bgp_smart_contracts/src/proxy.py
+++ bgp_smart_contracts/src/proxy.py
@@ -14,7 +14,7 @@ from Classes.PacketProcessing.Index import Index
 from Classes.PacketProcessing.ConnectionTracker import ConnectionTracker
 from Classes.PacketProcessing.FiveTuple import FiveTuple
 from Classes.PacketProcessing.FlowDirection import FlowDirection
-from Classes.PacketProcessing.DatabaseValidation import db_validate
+#from Classes.PacketProcessing.DatabaseValidation import db_validate
 from ipaddress import IPv4Address
 from Classes.Account import Account
 import os, sys
@@ -171,7 +171,33 @@ def remove_invalid_nlri_from_packet(m_pkt, nlri, update):
     else:
         print("ERROR: packet modification failed")
 
+def db_validate(segment):
 
+    inIP = IPv4Address(segment[1])
+    inSubnet = int(segment[2])
+    inASN = int(segment[0])
+    print ("Validating segment: AS" + str(inASN)+ " , " + str(inIP) + "/" + str(inSubnet))
+    
+    # tx_sender.generate_transaction_object("IANA", "IANA_CONTRACT_ADDRESS")
+    # print("Transaction setup complete for: " + tx_sender_name)
+    
+    ret = collection.find({'containers.labels.net_0_address': str(inIP) + "/" + str(inSubnet)})
+    
+    validASN = ""
+
+    for object in ret:
+        print(object)
+        validASN = object.get("asn")
+
+    if validASN == "":
+        print ("Prefix not registered")
+        return (validatePrefixResult.prefixNotRegistered)
+    elif validASN == inASN: #better approach
+        print ("Prefix is valid")
+        return (validatePrefixResult.prefixValid)
+    else:
+        print ("Owners don't match")
+        return (validatePrefixResult.prefixOwnersDoNotMatch)
 
 
 if __name__=='__main__':
